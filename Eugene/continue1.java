8c80b958b4abcdd2d87520d8bd86009775709d54
MUKESHIMANA928 <150811329+MUKESHIMANA928@users.noreply.github.com>
2023-11-21 20:36:51 +0100

    Add files via upload

---
+++ Eugene/continue1.java
@@ -0,0 +1,12 @@
+class ContinueExample {  
+public static void main(String[] args) {  
+    //for loop  
+    for(int z=1;z<=10;z++){  
+        if(z==7){  
+            //using continue statement  
+            continue;//it will skip the rest statement  
+        }  
+        System.out.println(z);  
+    }  
+}  
+}
---
+++ Eugene/continue2.java
@@ -0,0 +1,12 @@
+class ContinueExample {  
+public static void main(String[] args) {  
+    //for loop  
+    for(int z=1;z<=11;z++){  
+        if(z==10){  
+            //using continue statement  
+            continue;//it will skip the rest statement  
+        }  
+        System.out.println("this is continue example");  
+    }  
+}  
+}
